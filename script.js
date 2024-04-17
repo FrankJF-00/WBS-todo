0acceb0dd963637c8d7c312cba9cae738d0bd6a3
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-04-17 20:49:02 +0200

    trying object

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ const todoList = {
     todoInput.className = "todo-input";
     todoInput.placeholder = "add a tada";
     // Copy the text from the input field and paste it into the newly created input
-    const inputField = document.querySelector("input");
-    todoInput.value = inputField.value.trim(); // Trim the input value
+    todoInput.value = todoList.input.value.trim(); // Trim the input value
     // create delete button
     const deleteBtn = document.createElement("button");
     deleteBtn.className = "delete-btn";
@@ -30,20 +29,23 @@ const todoList = {
 
     // Add event listener to the delete button
     deleteBtn.addEventListener("click", function () {
-      newItem.remove(); // Remove the parent element (todo item)
+      // Remove the parent element (todo item)
+      newItem.remove();
     });
     // Add event listener to the checked button
+    function moveItem(action, position) {
+      checkBtn.classList[action]("check-btn-checked");
+      todoInput.classList[action]("input-checked");
+      deleteBtn.classList[action]("delete-btn-disabled");
+      listContainer[position](newItem);
+    }
     checkBtn.addEventListener("change", function () {
       if (checkBtn.checked) {
-        checkBtn.classList.add("check-btn-checked");
-        todoInput.classList.add("input-checked");
-        deleteBtn.classList.add("delete-btn-disabled");
-        listContainer.appendChild(newItem); // Move the todo item to the bottom of the main
+        // move todo item to the bottom
+        moveItem("add", "appendChild");
       } else {
-        checkBtn.classList.remove("check-btn-checked");
-        todoInput.classList.remove("input-checked");
-        deleteBtn.classList.remove("delete-btn-disabled");
-        listContainer.prepend(newItem); // Move the todo item back to the top of the main
+        // move todo item back to the top
+        moveItem("remove", "prepend");
       }
     });
   },
@@ -52,7 +54,7 @@ const todoList = {
 // function to handle adding a new todo item
 function handleAddTodo() {
   // check if the input field is empty
-  if (document.querySelector("input").value.trim() === "") {
+  if (todoList.input.value.trim() === "") {
     alert("Please add a todo!");
   } else {
     todoList.addItem();
@@ -60,13 +62,12 @@ function handleAddTodo() {
   }
 }
 
-// button to test functionality
 document
   .querySelector("#todo-add-btn")
   .addEventListener("click", handleAddTodo);
 
 // listen for keydown event on the input field
-document.querySelector("#todo-input").addEventListener("keydown", (event) => {
+todoList.input.addEventListener("keydown", (event) => {
   // check if the key pressed is ENTER
   if (event.key === "Enter") {
     handleAddTodo();
